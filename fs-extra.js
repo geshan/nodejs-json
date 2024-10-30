40bc1febb809bf659a3db198728bca9739274790
Geshan Manandhar <geshan@simplywallst.com>
2024-10-30 22:23:13 +1100

    Fix typo in billionaires

--- fs-extra.js
+++ fs-extra.js
@@ -1,7 +1,7 @@
 import { readJson } from 'fs-extra/esm';
 
 try {
-  const data = await readJson('billionares-2023.json');
+  const data = await readJson('billionaires-2023.json');
   console.log(data[1]); // Elon Musk's data
 }
 catch (err) {
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { readFile } from 'fs/promises';
+import { readFile } from 'node:fs/promises';
 
 try {
-  const data = JSON.parse(await readFile('billionares-2023.json', 'utf8'));
+  const data = JSON.parse(await readFile('billionaires-2023.json', 'utf8'));
   console.log(data[1]); //Elon Musk's data
 } catch (err) {
   console.error(`Error reading JSON file: ${err}`);
